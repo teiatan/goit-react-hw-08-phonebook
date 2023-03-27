722944554271feeb65c4e65c4a80f06ae5443e44
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-28 00:03:16 +0300

    make contacts slice

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -1,25 +1,34 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit';
 
-export const addContact = createAction('contacts/addContact');
-export const deleteContact = createAction('contacts/deleteContact');
 export const getFilter = createAction('filter/getFilter');
 
-const contactsReducer = createReducer([
+const contactsSlice = createSlice({
+    name: 'contacts',
+    initialState: [
         {name: 'Rosie Simpson', number: '459-12-56'},
         {name: 'Hermione Kline', number: '443-89-12'},
         {name: 'Eden Clements', number: '645-17-79'},
         {name: 'Annie Copeland', number: '227-91-26'},
-    ], {
-        [addContact]: (state, action) => [...state, {name: action.payload.name, number: action.payload.number}],
-        [deleteContact]: (state, action) => state.filter(contact => contact.name !== action.payload),
-    }
-);
+    ],
+    reducers: {
+        addContact(state, action) {
+            return [...state, {name: action.payload.name, number: action.payload.number}]
+        },
+        deleteContact(state, action) {
+            return state.filter(contact => contact.name !== action.payload)
+        },
+    },
+});
+
+
+export const { addContact, deleteContact } = contactsSlice.actions;
+
 const filterReducer = createReducer("", {
     [getFilter]: (state, action) => action.payload,
 })
 
 export const store = configureStore({ reducer: {
-    contacts: contactsReducer,
+    contacts: contactsSlice.reducer,
     filter: filterReducer,
 } });
