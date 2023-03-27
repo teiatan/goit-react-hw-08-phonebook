f8f8cec5f552661caede4afebe879e88f9a56e45
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-27 22:50:53 +0300

    refactored filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,20 +1,11 @@
-import { useState, useEffect } from 'react';
 import { Label, Input } from "./Filter.styled";
 import { useSelector, useDispatch } from 'react-redux';
 import { getFilter } from 'redux/store';
 
 export function Filter () {
 
-    //const [value, setValue] = useState("");
-
-    /* useEffect(()=>{
-        getFilterRequest(value);
-    }, [value, getFilterRequest]); */
     const value = useSelector(state => state.filter);
     const dispatch = useDispatch();
-    /* const onChange = e => {
-        setValue(e.currentTarget.value);
-    }; */
 
     return (
         <Label>
