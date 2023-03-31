e3dbeb27abaf280f9653156e110858241e08c652
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-31 22:59:38 +0300

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import { Contacts } from 'pages/Contacts';
+import { Layout } from './Layout/Layout';
+import { Login } from 'pages/Login';
+import { Register } from 'pages/Register';
 
 
 
@@ -7,7 +10,13 @@ export const App = () => {
   
   return (
     <>
-      <Contacts />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path='contacts' element={<Contacts />}/>
+          <Route path='login' element={<Login />}/>
+          <Route path='register' element={<Register />} />
+        </Route>
+      </Routes>
     </>
   );
 };
