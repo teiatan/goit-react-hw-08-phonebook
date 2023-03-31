211d65bb1e441848045a9d2a4cb719ac9efc5183
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-31 22:37:19 +0300

    add browser router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import { store } from 'redux/store';
@@ -8,7 +9,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter basename="goit-react-hw-05-movies">
         <App />
+      </BrowserRouter> 
     </Provider> 
   </React.StrictMode>
 );
