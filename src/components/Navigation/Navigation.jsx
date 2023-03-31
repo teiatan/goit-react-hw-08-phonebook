808d2d109bc84986b3f5eba0c4d0102391682343
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-31 23:03:43 +0300

    add navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom"
+
 export function Navigation() {
     return(
-        <p>Navigation</p>
+        <nav>
+            <ul>
+                <li><Link to="/contacts">contacts</Link></li>
+                <li><Link to="/login">login</Link></li>
+                <li><Link to="/register">register</Link></li>
+            </ul>
+        </nav>
     )
 };
