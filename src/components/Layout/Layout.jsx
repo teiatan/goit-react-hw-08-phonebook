13e20334d2a1d03c168f27c1b436ab54a89bdbc9
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-31 23:32:54 +0300

    add user menu

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,13 +2,15 @@ import { Suspense } from "react"
 import { Outlet } from "react-router-dom"
 import { Navigation } from "components/Navigation/Navigation"
 import { Loader } from "components/Loader/Loader"
+import { UserMenu } from "components/UserMenu/UserMenu"
 
 export function Layout({children}) {
 
     return (
         <>
             <header>
-            <Navigation /> 
+                <Navigation /> 
+                <UserMenu />
             </header>
             <main>
                 <Suspense fallback={<Loader />}>
---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,11 @@
+export function UserMenu() {
+    return (
+        <div>
+            <button>mobile button</button>
+            <ul>
+                <p>e-mail</p>
+                <button>sign out</button>
+            </ul>
+        </div>
+    )
+}
