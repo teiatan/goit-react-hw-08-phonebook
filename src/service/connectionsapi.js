9276f16a5e0ffb2daed76921395d4659f32de24a
teiatan <92535482+teiatan@users.noreply.github.com>
2023-04-01 17:43:26 +0300

    sign out without http errors

--- src/service/connectionsapi.js
+++ src/service/connectionsapi.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
+const setAuthHeader = token => {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+    axios.defaults.headers.common.Authorization = '';
+};
+
 //operations with contacts
 
 export async function fetchContacts() {
@@ -28,17 +36,19 @@ export async function updateContact(contactId) {
 
 export async function signUp(credentials) {
     const { data } = await axios.post('/users/signup', credentials);
+    setAuthHeader(data.token);
     return data;
 };
 
 export async function signIn(credentials) {
     const { data } = await axios.post('/users/login', credentials);
+    setAuthHeader(data.token);
     return data;
 };
 
-export async function signOut({name, number}) {
-    const data = await axios.post('/users/logout', { name, number });
-    return data;
+export async function signOut() {
+    await axios.post('/users/logout');
+    clearAuthHeader();
 };
 
 export async function getUserInfo() {
